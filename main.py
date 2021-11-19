d7ed289ce455ce9432f902126c7c8fb7b7dc87a3
Sascha Frank <sascha.frank.88@gmx.de>
2021-11-19 02:21:36 +0100

    updated main.py - line-numbers of span.ocrx_line in .json

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import threading
 import os
 import uuid
 import pdftotree
+import re
+
 from pdf2image import convert_from_path
 from bs4 import BeautifulSoup, BeautifulStoneSoup
 from lxml.html.clean import unicode
@@ -18,7 +20,7 @@ except ImportError:
 
 class PDFLoader(threading.Thread):
     running = True
-    type = "file"
+    type_ = "file"
     file = None
     file_ = None
     images = None
@@ -28,10 +30,11 @@ class PDFLoader(threading.Thread):
     datetime_file_prefix = None
     pdf_html = None
     pdf_html_parsed = None
-
-    def __init__(self, file, type=None, running=None):
-        if os.path.isfile(file):
-            self.file = file
+    file_prefix = "page_"
+    
+    def __init__(self, file_path, type=None, running=None):
+        if os.path.isfile(file_path):
+            self.file = file_path
             self.file_ = {
                 "name": os.path.basename(self.file),
                 "dir": os.path.dirname(self.file)
@@ -47,7 +50,7 @@ class PDFLoader(threading.Thread):
         self.data_path = os.path.join(self.base_path, "data")
 
         if type is not None and type != "file":
-            self.type = type
+            self.type_ = type
         if running is not None and self.running is not running:
             self.running = running
 
@@ -63,15 +66,94 @@ class PDFLoader(threading.Thread):
 
         else:
             return
+            
+    html_start_with_Artikel = []
+    html_start_with_SN = []
+    html_start_with_Number = []
+    
+    def get_linenumber_of_span_start(self, span_tag, html_):
+        search_ = span_tag.prettify().splitlines()[0]
+        html_lines = html_.splitlines()
+        
+        html_text = span_tag.get_text().strip() \
+                                .replace(r"\\n", " ") \
+                                .replace("\n", " ")
+                                
+        
+        marked_number = False
+        m_1 = re.search(r"^(\d+)", html_text, re.DOTALL)
+        if m_1 is not None:
+            # print(m_1.group(1))
+            marked_number = True
+                                
+        marked = False
+        if html_text.startswith("Artikel"):
+            marked = True
+        
+        marked_sn = False
+        if html_text.startswith("SN"):
+            marked_sn = True
+        
+        
+        found_i = 0
+        for i_line in range(0, len(html_lines)):
+            line = html_lines[i_line]
+            if search_ in line:
+                if marked is True:
+                    self.html_start_with_Artikel.append(i_line)
+                if marked_sn is True:
+                    self.html_start_with_SN.append(i_line)
+                if marked_number is True:
+                    self.html_start_with_Number.append(i_line)
+                return i_line
+        return False
 
     def write_page_contents_to_file(self):
-        if not os.path.isdir(os.path.join(self.data_path, self.datetime_file_prefix)):
-            os.mkdir(os.path.join(self.data_path, self.datetime_file_prefix))
-        file_prefix = "page_"
-        current_folder = os.path.join(self.data_path, self.datetime_file_prefix)
-        html_filename = (self.date_prefix() + self.uuid + "-" + file_prefix).rstrip("_page").rstrip("-") + ".html"
         if len(self.pdf_html) > 0:
             soup = BeautifulSoup(self.pdf_html, "lxml")
+            if not os.path.isdir(os.path.join(self.data_path, self.datetime_file_prefix)):
+                os.mkdir(os.path.join(self.data_path, self.datetime_file_prefix))
+            
+            current_folder = os.path.join(self.data_path, self.datetime_file_prefix)
+            html_filename = (self.date_prefix() + self.uuid + "-" + self.file_prefix).rstrip("_page").rstrip("-") + ".html"
+            json_filename = html_filename.rstrip(".html")+"_html.json"
+                
+            all_spans = soup.find_all("span", {"class": "ocrx_line"})
+            
+            span_contents_i = 0
+            found_contents_i = 0
+            span_contents = {}
+            
+            for span_i in range(0, len(all_spans)):
+                current_span = all_spans[span_i]
+                if len(current_span.get_text()) > 0:
+                    span_content = self.unicode_to_html_entities(current_span.get_text().strip() \
+                                .replace(r"\\n", " ") \
+                                .replace("\n", " "))
+                    
+                    line_number = self.get_linenumber_of_span_start(current_span, self.pdf_html)
+                    if line_number is not False:
+                        found_i = line_number
+                    else:
+                        sys.exit(1)
+                    if str(found_i) not in span_contents.keys():
+                        span_contents[str(found_i)] = span_content
+                    else:
+                        if span_contents[str(found_i)] != span_content:                        
+                            tmp_ = []
+                            tmp_.append(span_contents[str(found_i)])
+                            tmp_.append(span_content)
+                            span_contents[str(found_i)] = tmp_
+                        else:
+                            span_contents[str(found_i)] = span_content
+                
+                    span_contents_i += 1            
+            if span_contents_i > 0:
+                json_filename = html_filename.rstrip(".html")+"_spans_html.json"
+                with open(os.path.join(current_folder, json_filename), "w") as json_span_f:
+                    json_span_f.write(json.dumps(span_contents, indent=4))
+                    json_span_f.close()
+   
             all_tr = soup.find("table").find_all("tr")
             contents = {}
             for ri in range(0, len(all_tr)):
@@ -88,26 +170,32 @@ class PDFLoader(threading.Thread):
                                 .replace("\n", " ")
                             text_content = self.unicode_to_html_entities(uni)
                             contents[str(ri)][str(itd)] = text_content
-            json_filename = html_filename.rstrip(".html")+"_html.json"
+
             with open(os.path.join(current_folder, json_filename), "w") as json_f:
                 json_f.write(json.dumps(contents, indent=4))
                 json_f.close()
-
+            
             with open(os.path.join(current_folder, html_filename), "w") as html_f:
                 html_f.write(self.pdf_html)
                 html_f.close()
+            
+            
+            for p in self.pages.keys():
+                page_id = p
+                current_page = self.pages[page_id]
+                current_filename = datetime.now().strftime("%Y%m%d_%H%M%S") + "_" + \
+                                   self.uuid + "-" + self.file_prefix + str(page_id) + ".txt"
 
-        for p in self.pages.keys():
-            page_id = p
-            current_page = self.pages[page_id]
-            current_filename = datetime.now().strftime("%Y%m%d_%H%M%S") + "_" + \
-                               self.uuid + "-" + file_prefix + str(page_id) + ".txt"
-
-            with open(os.path.join(current_folder, current_filename), "w", encoding="utf-8") as current_page_f:
-                self.write_content_file_header(current_page_f)
-                current_page_f.write(current_page)
-                current_page_f.close()
-
+                with open(os.path.join(current_folder, current_filename), "w", encoding="utf-8") as current_page_f:
+                    self.write_content_file_header(current_page_f)
+                    current_page_f.write(current_page)
+                    current_page_f.close()
+                    
+        else:
+            print("no html")
+            #print(current_filename)
+            
+            
     def run(self) -> None:
         try:
             self.images = self.pdf_to_img()
@@ -156,11 +244,13 @@ class PDFLoader(threading.Thread):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 0:
         pdf_loader = PDFLoader(sys.argv[1])
         pdf_loader.start()
         pdf_loader.running = False
         pdf_loader.join()
+    else:
+        print("error: no input filepath specified")
 
 '''
 # img = cv2.imread('image.jpg')
